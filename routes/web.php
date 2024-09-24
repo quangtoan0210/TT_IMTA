c38af614d65b742f4649659f625c8f9ef8c969d6
toantqph38960 <quavang2k4@gmail.com>
2024-09-24 20:39:21 +0700

    finishCategories

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DanhMucController;
 use App\Http\Controllers\AuthController;
 use App\Http\Middleware\CheckRoleAdminMiddleware;
 use Illuminate\Support\Facades\Route;
@@ -26,13 +27,27 @@ Route::get('register', [AuthController::class, 'showFormRegister']);
 
 Route::post('register', [AuthController::class, 'register'])->name('register');
 Route::post('logout', [AuthController::class, 'logout'])->name('logout');
-Route::middleware('auth')->group(function(){
-    Route::get('/home',function(){
-        return view('home');
+
+
+
+// Route ADMIN
+Route::middleware(['auth', 'auth.admin'])->prefix('admins')
+    ->as('admins.')
+    ->group(function () {
+        Route::get('/dashboard', function () {
+            return view('admins.dashboard');
+        })->name('dashboard');
+
+        // Route Danh mục
+        Route::prefix('danhmucs')
+            ->as('danhmucs.')
+            ->group(function () {
+                Route::get('/',                 [DanhMucController::class, 'index'])->name('index');
+                Route::get('/create',           [DanhMucController::class, 'create'])->name('create');
+                Route::post('/store',           [DanhMucController::class, 'store'])->name('store');
+                Route::get('/show/{id}',        [DanhMucController::class, 'show'])->name('show');
+                Route::get('{id}/edit',         [DanhMucController::class, 'edit'])->name('edit');
+                Route::put('{id}/update',       [DanhMucController::class, 'update'])->name('update');
+                Route::delete('{id}/destroy',   [DanhMucController::class, 'destroy'])->name('destroy');
+            });
     });
-    Route::middleware('auth.admin')->group(function(){
-        Route::get('/admin',function(){
-            return 'Đây là trang Admin';
-        });
-    });
-});
